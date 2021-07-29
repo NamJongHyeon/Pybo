07857c2e0a40bfac7e0bcb26cec943dcfe522c05
NamJongHyeon <crjh8942@gmail.com>
2021-07-29 20:41:53 +0900

    pybo project

---
+++ pybo/templatetags/pybo_filter.py
@@ -0,0 +1,18 @@
+from django import template
+
+register = template.Library()
+
+import markdown
+from django.utils.safestring import mark_safe
+
+
+
+@register.filter
+def sub(value, arg):
+    return value - arg
+
+
+@register.filter()
+def mark(value):
+    extensions = ["nl2br", "fenced_code"]
+    return mark_safe(markdown.markdown(value, extensions=extensions))
